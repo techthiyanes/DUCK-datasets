18151cca8c9d8f4ed8d3a3c8d2569338a22f2a9b
Paula Vidas <paulavidas@gmail.com>
2023-01-24 00:00:03 +0100

    update donut dataset script

--- arxiv_src/utils/create_dataset_metadata.py
+++ arxiv_src/utils/create_dataset_metadata.py
@@ -3,28 +3,29 @@ import json
 import glob
 from tqdm import tqdm
 
-ROOT_DIR = ""  # directory where arxiv_pairs is located
-
+ROOT_DIR = ""  # Directory where arxiv_pairs is located.
+DATASET_NAME = "arxiv-pairs-dataset"
 SPLIT_FRAC = {
-    "train": 0.9,
-    "validation": 0.1,
+    "train": 0.995,
+    "validation": 0.005,
 }
 
 assert os.path.exists(os.path.join(ROOT_DIR, "arxiv_pairs"))
+arxiv_ids = sorted(os.listdir(os.path.join(ROOT_DIR, "arxiv_pairs")))
 
-if os.path.exists(os.path.join(ROOT_DIR, "dataset")):
-    os.system("rm -rf {}".format(os.path.join(ROOT_DIR, "dataset")))
+if os.path.exists(os.path.join(ROOT_DIR, DATASET_NAME)):
+    os.system("rm -rf {}".format(os.path.join(ROOT_DIR, DATASET_NAME)))
 
 split_start = {}
 split_end = {}
 acc_size = 0
 for split in SPLIT_FRAC:
-    os.makedirs(os.path.join(ROOT_DIR, "dataset", split))
+    os.makedirs(os.path.join(ROOT_DIR, DATASET_NAME, split))
     split_start[split] = acc_size
-    split_end[split] = int(acc_size + SPLIT_FRAC[split] * len(os.listdir(os.path.join(ROOT_DIR, "arxiv_pairs"))))
+    split_end[split] = int(acc_size + SPLIT_FRAC[split] * len(arxiv_ids))
     acc_size = split_end[split]
 
-for i, arxiv_id in enumerate(tqdm(os.listdir(os.path.join(ROOT_DIR, "arxiv_pairs")))):
+for i, arxiv_id in enumerate(tqdm(arxiv_ids)):
     arxiv_path = os.path.join(ROOT_DIR, "arxiv_pairs", arxiv_id)
     if not os.path.isdir(arxiv_path) or not os.path.exists(os.path.join(arxiv_path, "tex")) or not os.path.exists(os.path.join(arxiv_path, "png")):
         continue
@@ -33,8 +34,9 @@ for i, arxiv_id in enumerate(tqdm(os.listdir(os.path.join(ROOT_DIR, "arxiv_pairs
 
     if len(tex_files) != len(png_files):
         continue
-
-    for png_file in png_files:
+    
+    last_is_empty = False
+    for png_file in sorted(png_files):
         png_file_name = os.path.basename(png_file)
         tex_file_name = "out" + png_file_name.split("-")[1].split(".")[0].lstrip("0") + ".tex"
         tex_file = os.path.join(arxiv_path, "tex", tex_file_name)
@@ -43,8 +45,17 @@ for i, arxiv_id in enumerate(tqdm(os.listdir(os.path.join(ROOT_DIR, "arxiv_pairs
         with open(tex_file, "r") as f:
             tex_contents = f.read()
 
-        if not tex_contents.strip():  # skip empty text
+        if not tex_contents.strip():  # Skip empty text.
+            last_is_empty = True
             continue
+        if last_is_empty:
+            # Sometimes it happens that few consecutive pages' labels are empty
+            # (but the pages aren't actually empty), and all that text gets
+            # assigned to the next page. We skip those pages because the label
+            # isn't correct.
+            last_is_empty = False
+            continue
+        last_is_empty = False
         
         new_file_name = "_".join([arxiv_id, png_file_name])
         line = json.dumps({
@@ -53,15 +64,16 @@ for i, arxiv_id in enumerate(tqdm(os.listdir(os.path.join(ROOT_DIR, "arxiv_pairs
                 "gt_parse": {
                     "text_sequence": tex_contents
                 },
-                "gt_parse_file": os.path.join("arxiv_pairs", arxiv_id, "tex", tex_file_name)
+                "png_file": os.path.join("arxiv_pairs", arxiv_id, "png", png_file_name),
+                "tex_file": os.path.join("arxiv_pairs", arxiv_id, "tex", tex_file_name)
             })
         }) + "\n"
 
         for split in SPLIT_FRAC:
             if split_start[split] <= i < split_end[split]:
                 original_path = os.path.join("arxiv_pairs", arxiv_id, "png", png_file_name)
-                new_path = os.path.join("dataset", split, new_file_name)
+                new_path = os.path.join(DATASET_NAME, split, new_file_name)
                 os.makedirs(os.path.dirname(new_path), exist_ok=True)
                 os.symlink(os.path.relpath(original_path, os.path.dirname(new_path)), new_path)
-                with open(os.path.join(ROOT_DIR, "dataset", split, "metadata.jsonl"), "a") as f:
+                with open(os.path.join(ROOT_DIR, DATASET_NAME, split, "metadata.jsonl"), "a") as f:
                     f.write(line)
