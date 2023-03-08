d6a2f092d807ea03419c73543e0597527b3891f6
Tomohiro Sawada <103265438+tsawada-math@users.noreply.github.com>
2023-03-08 07:01:27 +0100

    Update run.sh

--- jsonl_interface_math_physics/run.sh
+++ jsonl_interface_math_physics/run.sh
@@ -1,1 +1,1 @@
-python app.py --problems_file  /Users/tomohirosawada/Desktop/DUCK-datasets/jsonl/math/brayman.jsonl
+python app.py --problems_file  /Users/tomohirosawada/Desktop/DUCK-datasets/jsonl/math/berkeley.jsonl
