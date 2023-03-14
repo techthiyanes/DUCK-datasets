b8649923b03085292a92d3fb83edd6508393e932
Tomohiro Sawada <103265438+tsawada-math@users.noreply.github.com>
2023-03-14 06:01:56 +0100

    Interface for sorting through math/physics jsonls

---
+++ jsonl_math_physics_interface/README.md
@@ -0,0 +1,36 @@
+Interface for viewing JSONL files from dataset.
+
+This script is based off of [jsonl interface](https://github.com/TheDuckAI/DUCK-datasets/blob/main/jsonl_interface/README.md) and [annotation tool script](https://github.com/TheDuckAI/DUCK-datasets/tree/main/annotation_tool). 
+
+## Requirements
+You need to install `flask`.
+```bash
+pip install flask
+```
+
+## Usage
+The script accepts the following arguments:
+- `--input_file`: The path to the `.jsonl` file that contains the problems, e.g. `physics_books/data/em_quals_output.jsonl`
+- `--numerical_file`: The path to the `.jsonl` file where the numerical problems without images will be stored. 
+- `--numerical_images_file`: The path to the `.jsonl` file where the numerical problems with images will be stored. 
+- `--symbolic_file`: The path to the `.jsonl` file where the symbolic problems without images will be stored. 
+- `--symbolic_images_file`: The path to the `.jsonl` file where the symbolic problems with images will be stored. 
+
+
+To run the script, use the following command:
+```bash
+python app.py --problems_file PATH --numerical_file PATH --numerical_images_file PATH --symbolic_file PATH --symbolic_images_file PATH 
+```
+
+You should see a message like
+```bash
+ * Running on http://127.0.0.1:5000
+```
+You can then open the link in your browser.
+
+The file counter.txt keeps track of the line number of your current location in the input_file. 
+
+
+## Note. 
+
+The script assumes that the values for "Image" in the jsonl file are mathpix urls. 
---
+++ jsonl_math_physics_interface/app.py
@@ -0,0 +1,224 @@
+import argparse
+import itertools
+import json
+import random
+import pandas as pd
+
+from flask import Flask, render_template, request
+
+
+PROBLEM_FIELDS = [
+    "Topic",
+    "Book",
+    "Problem Statement",
+    "Answer Candidates",
+    "Images",
+    "Solution",
+    "Final Answer",
+]
+
+# utils
+def list_to_str(l):
+    return "\n".join(l)
+
+def str_to_list(s):
+    l = [x.strip() for x in s.split("\n")]
+    l = [x for x in l if x]
+    return l
+
+def read_jsonl(inp, l=None): 
+    # read jsonl file 
+    # return list of json objects
+    # if line is specified, return the json object at that line
+    json_list = []
+    with open(inp, 'r') as f:
+        for line in f:
+            json_list.append(json.loads(line))
+    if l:
+        return json_list[l]
+    else:
+        return json_list
+    
+def write_jsonl(inp,line, key, value, output_file=None):
+    # write jsonl file 
+    # if line is specified, write the json object at that line
+    # if output_file is not specified, overwrite input_file
+    with open(inp, 'r') as f:
+        json_objects = []
+        for json_line in f:
+            json_objects.append(json.loads(json_line))
+    
+    json_objects[line][key] = value
+    
+    if output_file: out = output_file
+    else: out = inp
+
+    with open(out, 'w') as f:
+        for json_obj in json_objects:
+            json.dump(json_obj, f)
+            f.write('\n')
+
+def append_jsonl(inp, json_obj, output_file=None):
+    # append json object to jsonl file
+    # if output_file is not specified, overwrite input_file
+    if output_file: out = output_file
+    else: out = inp
+
+    print(f"inp = {inp}")
+    print(f"json_obj = {json_obj}")
+    print(f"out = {out}")
+    with open(out, 'a') as f:
+        json.dump(json_obj, f)
+        f.write('\n')
+
+def update_index(file='counter.txt',value=None): 
+    # increase key in counter.txt
+    # if value is specified, set key to value
+    with open(file, 'r') as f:
+        ind=int(f.read())
+    if value:
+        with open(file, 'w') as f:
+            f.write(str(value))
+    else: 
+        with open(file, 'w') as f:
+            f.write(str(ind+1))
+
+
+def jsonl_length(file):
+    # return length of jsonl file
+    with open(file, 'r') as f:
+        return sum(1 for line in f)
+    
+
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def index(): 
+    if request.args:
+        problem = {
+            "Topic": request.args.get("topic"),
+            "Book": request.args.get("book"),
+            "Problem Statement": request.args.get("problem_statement"),
+            "Answer Candidates": request.args.get("answer_candidates"),
+            "Images": request.args.get("images"),
+            "Output Format Instructions": request.args.get("output_format_instructions"),
+            "Solution": request.args.get("solution"),
+            "Final Answer": request.args.get("final_answer"),
+        }
+        for field in problem:
+            problem[field] = problem[field].replace("\r", "")
+        problem["Answer Candidates"] = str_to_list(problem["Answer Candidates"])
+        problem["Images"] = str_to_list(problem["Images"])
+
+        # if not problem["Answer Candidates"]:
+        #     problem.pop("Answer Candidates")
+        # if not problem["Images"]:
+        #     problem.pop("Images")
+
+        if request.args.get("numerical"):
+            # use request.args.get(<name of input>) to 
+            # add the input to the jsonl file
+            if problem["Images"]: 
+                # add problem to numerical_images_file
+                append_jsonl(args.numerical_images_file, problem)
+                update_index()
+            else: 
+                # add problem to numerical_file
+                append_jsonl(args.numerical_file, problem)
+                update_index()
+        elif request.args.get("symbolic"):
+            if problem["Images"]: 
+                # add problem to symbolic_images_file
+                append_jsonl(args.symbolic_images_file, problem)
+                update_index()
+            else: 
+                # add problem to symbolic_file
+                append_jsonl(args.symbolic_file, problem)
+                update_index()
+        else:
+            raise ValueError("Unknown action")
+
+    # read key from counter.txt
+    ind = int(open('counter.txt', 'r').read())
+
+    if ind > len(input_file) - 1:
+        return "<p>There are no more problems!</p>"
+    
+    problem = input_file[ind]
+    
+    for field in PROBLEM_FIELDS:
+        if field not in problem:
+            problem[field] = ""
+
+    return render_template(
+        "template.html",
+        id=ind,
+        topic=problem["Topic"],
+        book=problem["Book"],
+        problem_statement=problem["Problem Statement"],
+        answer_candidates=list_to_str(problem["Answer Candidates"]),
+        images=list_to_str(problem["Images"]),
+        solution=problem["Solution"],
+        final_answer=problem["Final Answer"],
+        numerical_problems_cnt = jsonl_length(args.numerical_file),
+        numerical_problems_with_images_cnt = jsonl_length(args.numerical_images_file),
+        symbolic_problems_cnt = jsonl_length(args.symbolic_file),
+        symbolic_problems_with_images_cnt = jsonl_length(args.symbolic_images_file),
+        remaining_problems_cnt = len(input_file) - ind - 1,
+    )
+
+
+@app.after_request
+def add_header(response):
+    response.cache_control.no_store = True
+    return response
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input_file', type=str, required=True)
+parser.add_argument('--numerical_file', type=str, required=True)
+parser.add_argument('--numerical_images_file', type=str, required=True)
+parser.add_argument('--symbolic_file', type=str, required=True)
+parser.add_argument('--symbolic_images_file', type=str, required=True)
+# parser.add_argument('--show_images', action="store_true")
+# parser.add_argument('--show_choices', action="store_true")
+args = parser.parse_args()
+
+
+
+if args.input_file.endswith(".jsonl"):
+    input_file_name = args.input_file
+    input_file = read_jsonl(args.input_file)
+    numerical = read_jsonl(args.numerical_file)
+    numerical_images = read_jsonl(args.numerical_images_file)
+    symbolic = read_jsonl(args.symbolic_file)
+    symbolic_images = read_jsonl(args.symbolic_images_file)
+
+    with open(input_file_name, 'r') as json_file:
+        json_list = list(json_file)
+
+
+    for i in range(len(json_list)):
+        json_str = json_list[i]
+        result = json.loads(json_str)
+        
+        
+else: 
+    raise ValueError("Problems file must be JSONL file")
+
+
+
+
+print("Reading problems from", input_file_name)
+print("Write numerical problems to: {}".format(args.numerical_file))
+print("Writeg numerical images to: {}".format(args.numerical_images_file))
+print("Write symbolic problems to: {}".format(args.symbolic_file))
+print("Write symbolic images to: {}".format(args.symbolic_images_file))
+
+if __name__ == "__main__":
+    app.run()
+
