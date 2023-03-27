b00fe1ed0554830457bd059ae5992f7e4ed4716d
tsawada-math <tomosawada98@gmail.com>
2023-03-26 22:01:26 -0400

    Revised numerical.jsonl

--- jsonl_final_edit_interface/scripts/math_numerical.sh
+++ jsonl_final_edit_interface/scripts/math_numerical.sh
@@ -1,2 +1,2 @@
-python app.py --input_file  '/Users/tomohirosawada/Desktop/DUCK-datasets/final/data/valid/math/numerical.jsonl' --out_file math_numerical.jsonl 
+python3 app.py --input_file  '/Users/tomohirosawada/Desktop/DUCK-datasets/final/data/valid/math/numerical.jsonl' --out_file '/Users/tomohirosawada/Desktop/DUCK-datasets/final/data/valid/math/math_numerical.jsonl'
 
